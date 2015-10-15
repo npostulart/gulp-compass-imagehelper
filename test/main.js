1facb32cd479a61664f0eaae43bb8e461e2533d5
Niklas Postulart <npostulart@uandi.com>
2015-10-15 09:42:09 +0200

    Mocha Test Setup

---
+++ test/main.js
@@ -0,0 +1,24 @@
+var imageHelper = require('../');
+var assert = require('assert');
+
+describe('gulp-compass-imagehelper', function() {
+
+  before(function(done) {
+    done();
+  });
+
+  after(function(done) {
+    done();
+  });
+
+  describe('imageHelper()', function() {
+    it('should throw, when path is missing', function() {
+      assert.equal(true, false);
+    });
+
+    it('should log notice if svg dimensions not available', function() {
+      assert.equal(true, false);
+    });
+  });
+
+});
